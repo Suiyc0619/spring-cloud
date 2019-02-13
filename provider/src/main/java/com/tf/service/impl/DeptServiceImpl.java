e9439d318f7459bf9384ed7bbee8510d2f24e244
sui <sui0619@163.com>
2019-02-13 17:47:40 +0800

    cloud第一天

---
+++ provider/src/main/java/com/tf/service/impl/DeptServiceImpl.java
@@ -0,0 +1,36 @@
+package com.tf.service.impl;
+
+import java.util.List;
+
+import com.tf.dao.DeptDao;
+import com.tf.entity.Dept;
+import com.tf.service.DeptService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class DeptServiceImpl implements DeptService
+{
+	@Autowired
+	private DeptDao dao;
+	
+	@Override
+	public boolean add(Dept dept)
+	{
+		return dao.addDept(dept);
+	}
+
+	@Override
+	public Dept get(Long id)
+	{
+		return dao.findById(id);
+	}
+
+	@Override
+	public List<Dept> list()
+	{
+		return dao.findAll();
+	}
+
+}
