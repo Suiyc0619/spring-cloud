1f41102062ce4dd49d1916b23e90285c604d4f97
Suiyc <sui0619@163.com>
2019-04-17 22:38:17 +0800

    2

--- eureka/src/main/java/com/tf/Eureka7001.java
+++ eureka/src/main/java/com/tf/Eureka7001.java
@@ -10,6 +10,6 @@ public class Eureka7001 {
 
     public static void main(String[] args) {
         SpringApplication.run(Eureka7001.class,args);
-        System.out.println(1);
+        System.out.println(2);
     }
 }
